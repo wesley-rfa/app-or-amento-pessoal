31b0ae629caec516b6f4247f4b1f0fde942f30f0
Wesley Ribeiro <wesleyaraujo@cc.ci.ufpb.br>
2021-05-01 22:53:35 -0300

    salvando despesa em localStorage

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-function cadastrarDespesa(){
+
+
+class Bd {
+    constructor(){
+        let id = localStorage.getItem('id')
+
+        if(id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId)+1
+    }
+    //---Grava a despesa em local storage
+    gravar(despesa) {
+        let id = this.getProximoId()
+        localStorage.setItem('despesa' + id, JSON.stringify(despesa))
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+//---Recupera dados do form e grava
+function cadastrarDespesa() {
     let despesa = {
         ano: $('#ano').val(),
         mes: $('#mes').val(),
@@ -7,5 +32,5 @@ function cadastrarDespesa(){
         descricao: $('#descricao').val(),
         valor: $('#valor').val()
     }
-    console.log(despesa)
+    bd.gravar(despesa)
 }
